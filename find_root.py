d575a83fcb17fe6508400a802242b433547568df
thaidv96 <thaidv96@gmail.com>
2019-08-19 10:07:43 +0700

    counter by chunk

---
+++ find_root.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from multiprocessing import Pool, cpu_count
+import sys
+from collections import Counter
+import pickle
+path = sys.argv[1]
+chunksize = sys.argv[2]
+dfs = pd.read_csv(path, chunksize=chunksize)
+start_time = datetime.now()
+res = Counter()
+for i, df in enumerate(dfs):
+    running_time = datetime.now()
+    print("completed:", round(i*chunksize/120e4, 4), '%', 'estimated remaining:',
+          (running_time-start_time)/((i + 1)*chunksize) * 120e6 - (running_time-start_time))
+    true_idx = [i.split() for i in df.Fingerprint.values]
+    true_idx = [i for o in true_idx for i in o]
+    c = Counter(true_idx)
+    res += c
+
+
+with open('root_counter.pickle', 'wb') as handle:
+    pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
